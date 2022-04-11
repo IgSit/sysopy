ac8b133f654b74975a25ecc1bcd9cab7ff70b18d
igsit <sigors100@gmail.com>
2022-04-11 15:22:29 +0200

    lab 5 any 60%

--- lab5/zad1/main.c
+++ lab5/zad1/main.c
@@ -6,13 +6,13 @@
 
 #define MAX_ARGS 20
 #define MAX_LINES 10
-#define READ 0
-#define WRITE 1
-
-char* DICT[MAX_LINES];
+#define MAX_SIZE 256
+int READ = 0;
+int WRITE = 1;
 int FREE = 0;
+char* DICT[MAX_ARGS];
 
-char** parse_commands(char* line){
+char** parse_commands(char* line){  // split sth like cmd1 = cat /etc | echo "xd" | ls
     char** commands = (char**)calloc(MAX_ARGS, sizeof(char*));
     strtok(line, "=");
     char* seq;
@@ -21,8 +21,8 @@ char** parse_commands(char* line){
     return commands;
 }
 
-char** parse_command_args(char* command){
-    char** args = (char**)calloc(256, sizeof(char*));
+char** parse_command_args(char* command){  // split sth like ls -l /etc/passwd
+    char** args = (char**)calloc(MAX_SIZE, sizeof(char*));
     char* arg = strtok(command, " ");
     int cnt = 0;
     args[cnt++] = arg;
@@ -38,7 +38,7 @@ int get_line_num(char* line){
     return -1;
 }
 
-int* get_lines_to_execute(char* line){
+int* get_lines_to_execute(char* line){  // decide which commands should be executed from cmd1 | cmd2 etc
     char** lines = (char**)calloc(MAX_ARGS, sizeof(char*));
     char* arg = strtok(line, "|");
     int ctr = 0;
@@ -59,20 +59,78 @@ int* get_lines_to_execute(char* line){
 }
 
 void add_new_command(char* line, char** lines, int line_ctr) {
-    char *line_copy = (char *) calloc(256, sizeof(char));
+    char *line_copy = (char *) calloc(MAX_SIZE, sizeof(char));
     strcpy(line_copy, line);
     lines[line_ctr] = line_copy;
-    char *line_copy2 = (char *) calloc(256, sizeof(char));
+    char *line_copy2 = (char *) calloc(MAX_SIZE, sizeof(char));
     strcpy(line_copy2, line);
     char* word = strtok(line_copy2, " ");
     DICT[FREE++] = word;
 }
 
 
-void parse_execute(FILE* file){
+void manage_child_pipes(int pipe_in[2], int pipe_out[2], int i, int j, int cmd_cnt,
+                  int lines_number, char** commands) {
+
+    close(pipe_out[READ]);
+    close(pipe_in[WRITE]);
+    if (j == 0 && i == 0) { // first line & first command
+        dup2(pipe_out[WRITE], STDOUT_FILENO);
+    }
+    else if (j == cmd_cnt - 1 && i == lines_number - 1) { // last line & last command
+        close(pipe_out[WRITE]);
+        dup2(pipe_in[READ], STDIN_FILENO);
+    }
+    else {
+        dup2(pipe_in[READ], STDIN_FILENO);
+        dup2(pipe_out[WRITE], STDOUT_FILENO);
+    }
+
+    char path[MAX_SIZE];
+    char** args = parse_command_args(commands[j]);
+    strcpy(path, args[0]);
+    execvp(path, args);
+}
+
+void manage_parent_pipes(int pipe_in[2], int pipe_out[2]) {
+    close(pipe_in[WRITE]);
+    pipe_in[READ] = pipe_out[READ];
+    pipe_in[WRITE] = pipe_out[WRITE];
+    pipe(pipe_out);
+}
+
+void parse_execute_line(char** lines, char* line) {
+    int* lines_num = get_lines_to_execute(line);
+    int pipe_in[2];
+    int pipe_out[2];
+    pipe(pipe_out);
+
+    int lines_number = 0;
+    while(lines_num[lines_number] != -1) lines_number++;
+
+    for (int i = 0; i < lines_number; i++) {
+        char** commands = parse_commands(lines[lines_num[i]]);
+        int cmd_cnt = 0;
+        while (commands[cmd_cnt] != NULL) {
+            printf("command: %s\n", commands[cmd_cnt]);
+            cmd_cnt++;
+        }
+
+        printf("-----------OUTPUT-----------\n\n");
+        for (int j = 0; j < cmd_cnt; j++) {
+            pid_t pid = fork();
+            if (pid == 0)
+                manage_child_pipes(pipe_in, pipe_out, i, j, cmd_cnt, lines_number, commands);
+            else
+                manage_parent_pipes(pipe_in, pipe_out);
+        }
+    }
+    while ((wait(0)) > 0);
+}
+
+void parse_execute_file(FILE* file){
     char** lines = (char**)calloc(MAX_LINES, sizeof(char*));
     char* line = (char*)calloc(256, sizeof(char));
-    int* lines_num;
     int line_ctr = 0;
 
     while(fgets(line, 256 * sizeof(char), file)) {
@@ -84,60 +142,9 @@ void parse_execute(FILE* file){
             line_ctr++;
         }
         else {
-            lines_num = get_lines_to_execute(line);
-
-            int pipe_in[2];
-            int pipe_out[2];
-
-            pipe(pipe_out);
-
-            int lines_number = 0;
-            while(lines_num[lines_number] != -1) lines_number++;
-
-            for (int i = 0; i < lines_number; i++) {
-                char** commands = parse_commands(lines[lines_num[i]]);
-                int m = 0;
-                while (commands[m] != NULL) {
-                    printf("command: %s\n", commands[m]);
-                    m++;
-                }
-
-                printf("-----------OUTPUT-----------\n\n");
-                for (int j = 0; j < m; j++) {
-                    pid_t pid = fork();
-
-                    if (pid == 0) {
-                        close(pipe_out[READ]);
-                        close(pipe_in[WRITE]);
-                        if (j == 0 && i == 0) { // first line & first command
-                            dup2(pipe_out[WRITE], STDOUT_FILENO);
-                        }
-                        else if (j == m - 1 && i == lines_number - 1) { // last line & last command
-                            close(pipe_out[WRITE]);
-                            dup2(pipe_in[READ], STDIN_FILENO);
-                        }
-                        else {
-                            dup2(pipe_in[READ], STDIN_FILENO);
-                            dup2(pipe_out[WRITE], STDOUT_FILENO);
-                        }
-
-                        char path[256];
-                        char** args = parse_command_args(commands[j]);
-                        strcpy(path, args[0]);
-                        execvp(path, args);
-                    }
-                    else {  // non-child
-                        close(pipe_in[WRITE]);
-                        pipe_in[READ] = pipe_out[READ];
-                        pipe_in[WRITE] = pipe_out[WRITE];
-                        pipe(pipe_out);
-                    }
-                }
-            }
+            parse_execute_line(lines, line);
         }
-        while ((wait(0)) > 0);
     }
-
 }
 
 int main(int argc, char* argv[]){
@@ -147,6 +154,6 @@ int main(int argc, char* argv[]){
     }
     char* path = argv[1];
     FILE* commands = fopen(path, "r");
-    parse_execute(commands);
+    parse_execute_file(commands);
     fclose(commands);
 }
--- lab5/zad2/main.c
+++ lab5/zad2/main.c
@@ -2,75 +2,53 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_SIZE 256
+int MAX_SIZE = 256;
 
-void print_output(FILE* file){
+void send_email(char* address, char* topic, char* content){
+    char command[MAX_SIZE];
+    snprintf(command, sizeof(command), "echo %s | mail %s -s %s", content, address, topic);
+    printf("%s\n", command);
+    popen(command, "r");
+
+    printf("\nEmail sent!\n");
+    printf("TO:      %s\nTopic:  %s\nCONTENT: %s\n", address, topic, content);
+
+}
+
+void print_ordered(char* mode){
+    char* command;
+
+    if (strcmp(mode,"date") == 0) {
+        command = "echo | mail -H";
+    }
+    else if (strcmp(mode, "sender") == 0){
+        command = "echo | mail -H | sort -k 3";
+    }
+    else{
+        printf("Wrong mode.\n");
+        exit(1);
+    }
+
+    FILE* file = popen(command, "r");
+    printf("\nSORTED EMAILS:\n");
     char line[MAX_SIZE];
     while(fgets(line, MAX_SIZE, file) != NULL){
         printf("%s\n", line);
     }
 }
 
-void print_ordered_by(char* mode){
-    FILE* file;
-    char* command;
-
-    if (strcmp(mode,"date") == 0) {
-        command = "echo | mail -f | tac";
-    }
-    else if (strcmp(mode, "sender") == 0){
-        command = "echo | mail -f | sort -k 2";
-    }
-    else{
-        printf("Wrong mode! Choose: [date] or [sender].\n");
-        exit(1);
-    }
-
-    file = popen(command, "r");
-
-    if (file == NULL){
-        printf("popen() error!");
-        exit(1);
-    }
-
-    printf("\nSORTED EMAILS: ");
-    print_output(file);
-}
-
-void send_email(char* address, char* object, char* content){
-    FILE* file;
-    char command[MAX_SIZE];
-
-    snprintf(command, sizeof(command), "echo %s | mail -s %s %s", content, object, address);
-    printf("%s\n", command);
-    file = popen(command, "r");
-
-    if (file == NULL){
-        printf("popen() error!");
-        exit(1);
-    }
-
-    printf("\n----- Email sent! -----\n");
-    printf("TO:      %s\nOBJECT:  %s\nCONTENT: %s\n", address, object, content);
-
-}
-
 int main(int argc, char* argv[]){
     if (argc != 2 && argc != 4){
-        printf("Wrong number of arguments!");
-        exit(1);
+        printf("Wrong number of arguments.");
+        return -1;
     }
     if (argc == 2){
-        printf("Print emails ordered by %s.\n", argv[1]);
-        print_ordered_by(argv[1]);
+        print_ordered(argv[1]);
+        return 0;
     }
-    else {
-        printf("Send email.\n");
-        char* address = argv[1];
-        char* subject = argv[2];
-        char* content = argv[3];
-
-        send_email(address, subject, content);
-    }
-
+    char* address = argv[1];
+    char* topic = argv[2];
+    char* content = argv[3];
+    send_email(address, topic, content);
+    return 0;
 }
